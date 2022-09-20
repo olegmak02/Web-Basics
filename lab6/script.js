e23fe25c2c4c32e9a8c4e78c0c8eca8b5fe9400a
olegmak02 <olleglion02@gmail.com>
2022-09-20 23:57:12 +0300

    Files for lab 2 were changed

--- lab6/script.js
+++ lab6/script.js
@@ -5,13 +5,32 @@ function clickHandler(event) {
         .then((data) => data.json())
         .then((data) => {
             console.log(data);
+            const info = document.getElementById("info");
+            info.innerHTML = '';
+
             const photo = document.createElement("img");
             photo.src = data.results[0].picture.large;
-            photo.style.width = "50%";
-            photo.style.marginLeft = "25%";
+            photo.style.marginLeft = "30%";
             console.log(data.results);
-            document.getElementById("info").appendChild(photo);
-            "picture, phone, coordinates, postcode, country"
+            info.appendChild(photo);
+
+            const phone = document.createElement("p");
+            phone.innerText = data.results[0].phone;
+            info.appendChild(phone);
+
+            const coordinates = document.createElement("p");
+            coordinates.innerText = "широта: " + data.results[0].location.coordinates.latitude + 
+                "довгота: " + data.results[0].location.coordinates.longitude;
+
+            info.appendChild(coordinates);
+
+            const postcode = document.createElement("p");
+            postcode.innerText = data.results[0].location.postcode;
+            info.appendChild(postcode);
+
+            const country = document.createElement("p");
+            country.innerText = data.results[0].location.country;
+            info.appendChild(country);
         });
 }
 
